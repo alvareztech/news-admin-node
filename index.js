6100e4d291a586d7698e07d304af39c16fe696c4
Daniel Alvarez <a3dany@gmail.com>
2018-07-28 19:59:12 -0400

    Scraping Los Tiempos

--- index.js
+++ index.js
@@ -18,18 +18,84 @@ function saveNews() {
     db.collection("news").doc("news-1").set(data);
 }
 
-saveNews();
 
-const url = "https://alvarez.tech";
+const url = "http://www.lostiempos.com/ultimas-noticias";
 if (!url) {
     throw "Please provide a URL as the first argument";
 }
 
-async function run () {
+async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await page.screenshot({path: 'screenshot.png'});
+    await page.screenshot({path: 'screenshot2.png'});
     browser.close();
 }
-run();
+
+function runScrapeLosTiempos(pagesToScrape) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!pagesToScrape) {
+                pagesToScrape = 1;
+            }
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto("http://www.lostiempos.com/ultimas-noticias");
+            let currentPage = 1;
+            let posts = [];
+            while (currentPage <= pagesToScrape) {
+                console.log('************************************************** Page ' + currentPage);
+                let newPosts = await page.evaluate(() => {
+                    let results = [];
+                    let items = document.querySelectorAll('div.term-0');
+                    items.forEach((item) => {
+                        let titleDiv = item.querySelector('div.views-field-title');
+                        let titleA = titleDiv.querySelector('a');
+
+                        let summaryDiv = item.querySelector('div.views-field-field-noticia-sumario');
+                        let summary = summaryDiv.querySelector('span.field-content');
+
+                        let img = item.querySelector('img');
+
+                        let dateSpan = item.querySelector('span.date-display-single');
+
+                        let categorySpan = item.querySelector('span.views-field-seccion');
+
+                        results.push({
+                            title: titleA.innerText,
+                            url: titleA.getAttribute('href'),
+                            summary: summary.innerText,
+                            image: img.getAttribute('src'),
+                            date: dateSpan.getAttribute('content'),
+                            category: categorySpan.innerText
+                        });
+                    });
+                    return results;
+                });
+
+                posts = posts.concat(newPosts);
+                if (currentPage < pagesToScrape) {
+                    await Promise.all([
+                        // await page.click('a.morelink'),
+                        await page.goto('http://www.lostiempos.com/ultimas-noticias?page=' + currentPage),
+                        await page.waitForSelector('div.term-0')
+                    ])
+                }
+                currentPage++;
+
+            }
+            browser.close();
+            return resolve(posts);
+        } catch (e) {
+            return reject(e);
+        }
+    })
+}
+
+// saveNews();
+// run();
+runScrapeLosTiempos(2).then(value => {
+    console.log('Number of posts: ' + value.length);
+    console.log('Posts:');
+    console.log(value);
+}).catch(console.error);

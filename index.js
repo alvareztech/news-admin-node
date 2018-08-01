cc1aad6fa2102d3d36ee077f7820fce2f17c73c9
Daniel Alvarez <a3dany@gmail.com>
2018-07-31 22:47:05 -0400

    Start implementation P7

--- index.js
+++ index.js
@@ -115,11 +115,89 @@ function runScrapeLosTiempos(pagesToScrape) {
     })
 }
 
+function runScrapePaginaSiete(pagesToScrape) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!pagesToScrape) {
+                pagesToScrape = 1;
+            }
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto("https://www.paginasiete.bo/nacional/");
+            let currentPage = 1;
+            let posts = [];
+            while (currentPage <= pagesToScrape) {
+                console.log('************************************************** Page ' + currentPage);
+                let newPosts = await page.evaluate(() => {
+                    let results = [];
+                    let items = document.querySelectorAll('article');
+                    items.forEach((item) => {
+                        console.log(' >>> ');
+                        let titleDiv = item.querySelector('h2.titulo');
+                        console.log(' > ' + titleDiv);
+                        let titleA = titleDiv.querySelector('a');
+                        console.log(' > ' + titleA);
+
+                        // let summaryDiv = item.querySelector('div.views-field-field-noticia-sumario');
+                        // let summary = summaryDiv.querySelector('span.field-content');
+
+                        let picture = item.querySelector('picture');
+                        let img = "";
+                        let src = "";
+                        if (picture) {
+                            img = picture.querySelector('img');
+                            if (img) {
+                                src = img.getAttribute('src');
+                            }
+                        }
+
+                        // let dateSpan = item.querySelector('span.date-display-single');
+
+                        // let categorySpan = item.querySelector('span.views-field-seccion');
+
+                        results.push({
+                            title: titleA.innerText,
+                            url: titleA.getAttribute('href'),
+                            // summary: summary.innerText,
+                            image: src,
+                            // date: dateSpan.getAttribute('content'),
+                            // category: categorySpan.innerText
+                        });
+                    });
+                    return results;
+                });
+
+                posts = posts.concat(newPosts);
+                // if (currentPage < pagesToScrape) {
+                //     await Promise.all([
+                //         // await page.click('a.morelink'),
+                //         await page.goto('http://www.lostiempos.com/ultimas-noticias?page=' + currentPage),
+                //         await page.waitForSelector('div.term-0')
+                //     ])
+                // }
+                currentPage++;
+
+            }
+            browser.close();
+            return resolve(posts);
+        } catch (e) {
+            return reject(e);
+        }
+    })
+}
+
 // saveNews();
 // run();
-runScrapeLosTiempos(1).then(value => {
+// runScrapeLosTiempos(1).then(value => {
+//     console.log('Number of posts: ' + value.length);
+//     console.log('Posts:');
+//     console.log(value);
+//     saveFirebase(value);
+// }).catch(console.error);
+
+runScrapePaginaSiete(1).then(value => {
     console.log('Number of posts: ' + value.length);
     console.log('Posts:');
     console.log(value);
-    saveFirebase(value);
+    // saveFirebase(value);
 }).catch(console.error);
